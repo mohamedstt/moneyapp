5b2ac15d665223b90590883d69c16e4d34054efc
Mohamedstt <mohamedschof@gmail.com>
2021-05-28 13:51:30 -0300

    Header component created.

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -17,7 +17,7 @@ export const GlobalStyle = createGlobalStyle`
     
     * {
      margin: 0;
-     padding: 0:
+     padding: 0;
      box-sizing: border-box;
     }
     
@@ -36,7 +36,13 @@ export const GlobalStyle = createGlobalStyle`
      background: var(--background);
      -webkit-font-smoothing: antialiased;
     }
-
+    body, input,textarea,button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+    h1,h2,h3,h4,h5,h6,strong{
+        font-weight:600;
+    }
     button {
         cursor: pointer;
     }

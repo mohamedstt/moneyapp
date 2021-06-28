1dd7af9358b8e768f16dfbe1c7d591d5918facd0
Mohamedstt <mohamedschof@gmail.com>
2021-06-28 16:27:49 +0100

    Transactions list

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransationModal } from "./components/NewTransationModal";
+import { TransactionsProvider } from "./TransactionsContext";
 import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement("#root");
@@ -17,7 +18,7 @@ export function App() {
     setIsNewTransactionModalOpen(false);
   }
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransationModal
@@ -25,6 +26,6 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
